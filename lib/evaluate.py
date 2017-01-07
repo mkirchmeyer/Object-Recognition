36c037824b14076958182affa3d06c67f213485b
syltruong <syltruong@gmail.com>
2017-01-07 15:36:48 +0100

    added code for avaluation and expression handling in glove

---
+++ lib/evaluate.py
@@ -0,0 +1,97 @@
+import numpy as np
+import sys
+from pycocotools.coco import COCO
+
+# threshold sampling
+thr_array = np.linspace(0,1,100)
+
+# input is a dictionary id:score
+def normalize_output(scores):
+    total = 0
+    for key in scores.keys():
+        total += scores[key]
+    for key in scores.keys():
+        scores[key] /= float(total)
+
+def id2tag(coco,ids):
+    # returns dict id:str_array
+    result = {}
+    for idx in ids:
+        annIds = coco.getAnnIds(imgIds=idx)
+        anns = coco.loadAnns(annIds)
+        strings = []
+        for ann in anns:
+            instance = coco.loadCats(ann['category_id'])[0]['name']
+            strings.append(instance)
+        result[idx] = strings
+    return result
+
+def evaluateROC(coco,tag2img,imgs_test):
+    # coco is the COCO API instance that was constructed from the categories .json
+    # tag2img is the function returning the scores on the test images
+    # imgs_test are the coco images on which evaluation is performed
+    # (be careful, the ids returned by tag2img and imgs_test have to be corresponding) 
+    # loop over possible category tags of the coco API instance
+    # why not put them all in a same object named imgQueryObject
+    
+    ids = np.zeros(len(imgs_test))
+    for k in range(len(imgs_test)):
+        ids[k] = imgs_test[k]['id']
+    #ids = []
+    #for img in imgs_test:
+    #    ids.append(img['id'])
+
+    GT = id2tag(coco,ids) # ground truth tags per image index
+
+    cats = coco.loadCats(coco.getCatIds())
+
+    precision_array = np.zeros(len(thr_array))
+    recall_array = np.zeros(len(thr_array))
+    
+    # loop over possible queries
+    for k in range(len(cats)):
+        cat = cats[k]
+        query = cat['name']
+        result = tag2img(query) # dictionnary id:score
+        normalize_output(result)
+        
+        pos_array = np.zeros(len(thr_array))
+        true_pos_array = np.zeros(len(thr_array))
+        
+        for thr in thr_array:
+            filtered_result = {idx: score for idx, score in result.iteritems() if score > thr}
+            for idx in filtered_result.keys():
+                pos_array[thr] += 1
+                if query in GT[idx]:
+                    true_pos_array[thr] += 1
+
+        precision_array = precision_array + true_pos_array / pos_array
+        recall_array = recall_array + true_pos_array / len({idx:categories from idx, categories in GT/iteritems() if (query in categories)})
+
+    # take the mean of all ROC curves over all possible queries
+    precision_array = precision_array / len(cats)
+    recall_array = recall_array / len(cats)
+
+    return precision_array, recall_array
+
+#def top_five_precision(coco,tag2Img,imgs_test):
+#    ids = np.zeros(len(imgs_test))
+#    for k in range(len(imgs_test)):
+#        ids[k] = imgs_test[k]['id']
+    #ids = []
+    #for img in imgs_test:
+    #    ids.append(img['id'])
+
+#    GT = id2tag(coco,ids) # ground truth tags per image index
+
+    
+
+def computeAP(precision_array, recall_array):
+    assert precision_array.shape == recall.array.shape, "in compute AP, shape mismatch"
+    
+    result = 0
+    for k in range(len(precision_array)-1):
+        result += precision_array[k] * (recall_array[k+1] - recall_array[k])
+    assert result <= 1 and result >= 0, "in computeAP, result non consistant"
+    
+    return result 
--- lib/glove.py
+++ lib/glove.py
@@ -6,14 +6,26 @@ def load_glove(vectors_file):
         for line in f:
             vals = line.rstrip().split(' ')
             vectors[vals[0]] = map(float,vals[1:])
-    return vectors
+    return vectors, len(vectors[vectors.keys()[0]])
     # all vector representations are now in vectors
 
 class glove():
     def __init__(self,path_to_glove_txt_file):
-        self.dict = load_glove(path_to_glove_txt_file)
+        self.dict, self.len_vectors = load_glove(path_to_glove_txt_file)
 
-    def vectorize(self,word):
+    def vec(self,word):
         if not word in self.dict.keys()):
             raise NameError('%s not in glove dictionnary' % word)
         return self.dict[word]
+    
+    def vec_expression(self,expression):
+        output = np.zeros(self.len_vectors)
+        expression = expression.rstrip().split()
+        n_words = 0
+        for word in expression:
+            if word in self.dict.keys():
+                n_words += 1
+                output = output + self.vec(word)
+        if n_words == 0:
+            raise NameError('\'%s\' not in glove dictionnary' % expression)
+        return output
