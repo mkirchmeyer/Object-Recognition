f6dceedf13ead651c395ef94677fc8d83028d209
14kirchm <matthieu.kirchmeyer@gmail.com>
2017-01-10 17:43:52 +0100

    Updates quantitative evaluation

--- lib/evaluate.py
+++ lib/evaluate.py
@@ -3,17 +3,9 @@ import sys
 #sys.path.append('/home/struong/Documents/MVA/S1/coco/PythonAPI/')
 sys.path.append('/media/matthieu/Documents/2016_2017_3A_Mines_Paristech/MVA/Recvis/Project/coco-master/PythonAPI/')
 from pycocotools.coco import COCO
-import skimage.io as io
-import operator
-import os
-import matplotlib.pyplot as plt
-from shutil import copyfile
-
-dataType = 'val2014'
-dataPath = '/home/struong/Documents/MVA/S1/coco/'
 
 # threshold sampling
-thr_array = np.linspace(0,1,500)
+thr_array = np.linspace(0,1,100)
 
 # input is a dictionary id:score
 def normalize_output(scores):
@@ -50,31 +42,6 @@ def id2tag(coco,ids):
             strings.append(instance)
         result[idx] = strings
     return result
-
-def retrieve(tag2img,file_id_dict,query,output_folder): # and display
-    result = tag2img(query) # dictionnary id:score
-    softmax(result)
-    spread(result)
-    best_images = sorted(result.iteritems(), key=operator.itemgetter(1), reverse=True)[:10]
-    fig = plt.figure()
-    plt.title(query)
-    plt.axis('off')
-
-    output_path = os.path.join(output_folder,query)
-    if not os.path.exists(output_path):
-        os.makedirs(output_path)
-
-    for i in range(len(best_images)):
-        # a = fig.add_subplot(2,5,i+1)
-        # plt.axis('off')
-        # I = io.imread(dataPath + 'images/' + dataType + '/' + file_id_dict[best_images[i][0]])
-        # plt.imshow(I)
-        # plt.tight_layout()
-        copyfile(dataPath + 'images/' + dataType + '/' + file_id_dict[best_images[i][0]],os.path.join(output_path,file_id_dict[best_images[i][0]]))
-    # plt.tight_layout()
-    # plt.savefig(os.path.join(output_folder,"%s.png"%query))
-    # print "Figure saved at: "+os.path.join(output_folder,"%s.png"%query)
-    return
 
 def tag2id(coco,ids):
     # returns dict cat:str_array of id
@@ -194,9 +161,6 @@ def evaluatePrecisionI2T(coco,img2tag,test_img,GT):
 
         result = img2tag(test_img[query]) # dictionnary cat:score
 
-        pos_array = np.zeros(len(thr_array))
-        true_pos_array = np.zeros(len(thr_array))
-
         #Display top number tags
         cats = result.keys()
         score = result.values()
@@ -207,16 +171,11 @@ def evaluatePrecisionI2T(coco,img2tag,test_img,GT):
 
         true_pos = 0
         for cat in top_cat:
-            print "GT"
-            print GT[cat]
-            print "query"
-            print query
             if query in GT[cat]:
                 true_pos += 1
 
-        precision[query] = true_pos / len(test_img)
+        precision[query] = float(true_pos) / len(test_img)
 
-        # esperance d'avoir le bon tag dans les 5 premiers scores precision top5 parcourir tous les imgaes test quand bon tage dans 5 premiers compte 1 sinon compte 0 somme / nb image. % precisoj dans top5
     print '\nComputed all queries!'
 
     return precision
